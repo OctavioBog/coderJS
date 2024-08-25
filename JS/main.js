f5bf12a90945e3a4babb8a5d38f438a7a596ee63
Octavio Luis Bogdan Guereschi <octavioluisbogdan96@hotmail.com>
2024-08-26 01:19:06 +0200

    seundo commit

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,32 @@
-// dato de la edad
 
 function getEdad() {
   let edad = 0;
-  while (edad <= 0 || edad === NaN) {
-    edad = parseInt(
-      prompt("ingresa tu edad. Recorda que debes ingresar un valor numérico")
-    );
+  while (edad <= 0 || edad == NaN) {
+    edad = parseInt(prompt("ingresa tu edad. Recorda que debes ingresar un valor numérico"));
+    console.log(edad)
   }
-
   return edad;
 }
 
-function getPeso() {}
+function getPeso() {
+  let peso = 0;
+  while (peso <= 0 || peso == NaN) {
+    peso = parseInt(prompt("ingresa tu peso en kg. Recorda que debes ingresar un valor numérico"));
+  }
 
-function getAltura() {}
+  return peso;
+  
+}
+
+function getAltura() {
+  
+  let altura = 0;
+  while (altura <= 0 || altura == NaN) {
+    altura = parseInt(prompt("ingresa tu altura en cm. Recorda que debes ingresar un valor numérico"));
+  }
+
+  return altura;
+}
 
 function getActividad() {
   let actividad = prompt(
@@ -59,17 +72,7 @@ function getGenero() {
   return genero;
 }
 
-const userEdad = getEdad();
-let peso = parseInt(prompt("ingresa tu peso en kg"));
-let altura = parseInt(prompt("ingresa tu altura en cm"));
-let actividad = parseFloat(
-  prompt(
-    "ingresa el numero que corresponda:\n1.25 nada de actividad \n1.50 un poco de actividad\n1.75 actividad moderada\n 2 actividad muy intensa"
-  )
-);
-let genero = prompt(
-  "ingresa segun corresponda:\n 1 para masculino\n 2 para femenino"
-);
+// Calculo de Calorias
 
 function calorias(edad, peso, altura, actividad, genero) {
   switch (genero) {
@@ -79,8 +82,18 @@ function calorias(edad, peso, altura, actividad, genero) {
       return 65 + 9.6 * peso + (1.8 * altura - 4.7 * edad) * actividad;
   }
 }
-let resultado = parseInt(calorias(edad, peso, altura, actividad, genero));
-alert("calorias diarias para bajar grasa corporal son: " + resultado);
+
+
+
+const userEdad = getEdad();
+const userPeso = getPeso();
+const userAltura = getAltura();
+const userActividad = getActividad();
+const userGenero = getGenero();
+
+
+let resultado = parseInt(calorias(userEdad, userPeso, userAltura, userActividad, userGenero));
+alert("calorias diarias para bajar de peso: " + resultado);
 
 
 
