d90a64c9e6d5c1b5c9cc976ca82753c074f5acc7
Octavio Luis Bogdan Guereschi <octavioluisbogdan96@hotmail.com>
2024-09-10 01:19:15 +0200

    segunda entrega

--- JS/main.js
+++ JS/main.js
@@ -1,104 +1,71 @@
+function ingresarproductos() {
+    let productos = [];
+    let continuar = true;
 
-function getEdad() {
-  let edad = parseInt(prompt("ingresa tu edad. Recorda que debes ingresar un valor numérico"));
+    while (continuar) {
+        const articulo = prompt("Ingresa el producto").toLowerCase();
+        
+        const productoExistente = productos.find(p => p.articulo === articulo);
+        if (productoExistente) {
+            alert("Este producto ya ha sido ingresado.");
+            continue;
+        }
 
-  if(typeof Number && edad > 0){
-    return edad;
-  }else{
-    return getEdad();
-  }
-}
+        const precio = parseFloat(prompt("Ingresa el precio del producto"));
+        if (isNaN(precio) || precio < 0) {
+            alert("Ingresa un precio válido");
+            continue;
+        }
 
-function getPeso() {
-  let peso = parseInt(prompt("ingresa tu peso en kg. Recorda que debes ingresar un valor numérico"));
+        productos.push({ articulo: articulo, precio: precio });
 
-  if(typeof Number && peso > 0){
-    return peso;
-  }else{
-    return getPeso();
-  }
-  
-}
+        let respuesta = prompt("¿Desea agregar algún producto más? SI/NO").toLowerCase();
+        while (respuesta !== "si" && respuesta !== "no") {
+            alert("Respuesta inválida, por favor ingrese SI o NO.");
+            respuesta = prompt("¿Desea agregar algún producto más? SI/NO").toLowerCase();
+        }
 
-function getAltura() {
-  
-  let altura = parseInt(prompt("ingresa tu altura en cm. Recorda que debes ingresar un valor numérico"));
-
-  if(typeof Number && altura > 0){
-    return altura;
-  }else{
-    return getAltura();
-  }
-}
-
-function getActividad() {
-  let actividad = prompt(
-    "ingresa el numero que corresponda: (A) nada de actividad (B) un poco de actividad, (C) actividad moderada, (D) actividad muy intensa"
-  );
-
-  switch (actividad) {
-    case "A":
-      return 1.25;
-      break;
-    case "B":
-      return 1.5;
-      break;
-    case "C":
-      return 1.75;
-      break;
-    case "D":
-      return 2;
-      break;
-    default:
-      getActividad();
-      break;
-  }
-}
-
-function getGenero() {
-  let genero = "";
-
-  while (genero !== "M" && genero !== "F" && genero !== "m" && genero !== "f") {
-    const g = prompt(
-      "ingresa segun corresponda:M para masculino, F para femenino"
-    );
-
-    if (g === "M" || g === "m") {
-      genero = "M";
+        if (respuesta === "no") {
+            continuar = false;
+        }
     }
 
-    if (g === "F" || g === "f") {
-      genero = "F";
+    return productos;
+}
+
+
+
+function cuotas(productos) {
+    const pagos = parseInt(prompt("Ingresa en cuántas cuotas quieres pagar. 1/3/6"));
+    const suma = productos.reduce((acc, producto) => acc + producto.precio, 0);
+    switch (pagos) {
+        case 1:
+            return suma;
+        case 3:
+            return suma / 3;
+        case 6:
+            return suma / 6;
+        default:
+            alert("Opción no válida. Mostrando el precio total.");
+            return suma;
     }
-  }
-
-  return genero;
+    
 }
 
-// Calculo de Calorias
+function mostrarResumen(productos) {
+    let resumen = "";
+    console.log('%c Resumen de la compra:' , 'color: red; font-weight: bold;');
+    productos.forEach(producto => {
+        resumen += `Producto: ${producto.articulo}, Precio: $${producto.precio.toFixed(2)}\n`;
+    });
 
-function calorias(edad, peso, altura, actividad, genero) {
-  switch (genero) {
-    case "M":
-      return 66 + 13.7 * peso + (5 * altura - 6.8 * edad) * actividad;
-    case "F":
-      return 65 + 9.6 * peso + (1.8 * altura - 4.7 * edad) * actividad;
-  }
+    const total = productos.reduce((acc, producto) => acc + producto.precio, 0);
+    resumen += `Total compra: $${total.toFixed(2)}`;
+    console.log(resumen);
 }
 
+let productos = ingresarproductos();
+let totalCuota = cuotas(productos);
+mostrarResumen(productos);
+console.log(`%c Total a pagar por cuota: $${totalCuota.toFixed(2)}`,'color: red; font-weight: bold;');
 
-
-const userEdad = getEdad();
-console.log(userEdad)
-const userPeso = getPeso();
-console.log(userPeso)
-const userAltura = getAltura();
-console.log(userAltura)
-const userActividad = getActividad();
-console.log(userActividad)
-const userGenero = getGenero();
-console.log(userGenero)
-
-
-let resultado = parseInt(calorias(userEdad, userPeso, userAltura, userActividad, userGenero));
-alert("calorias diarias para bajar de peso: " + resultado);
